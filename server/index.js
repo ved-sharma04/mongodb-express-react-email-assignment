bbf6782e1419e2a402a0a999307025e2a2b9266f
ved sharma <73155101+ved-sharma04@users.noreply.github.com>
2020-10-31 08:40:26 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const connectionString = 'mongodb+srv://dbuser:dbuser@cluster0.dm6ae.mongodb.net/mydb?retryWrites=true&w=majority';
+const connectionString = process.env.MONGODB_URI;
+//Mongodb Schema {email:"emailID"}
 
 app.use(cors());
 app.use(bodyParser.json());
